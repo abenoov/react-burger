6a0898922bef447df5aee20a3f47b4e04abe56b9
Mukhtar Abenov <mukhtarabenov@gmail.com>
2021-08-28 15:08:32 +0600

    Feat: API connection

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 import {AppHeader} from './app-header';
 import {BurgerIngredients} from './burger-ingredients';
 import {BurgerConstructor} from './burger-constructor';
 import styles from '../styles/app.module.css';
 
+const apiURL = "https://norma.nomoreparties.space/api/ingredients";
+
 export type AppProps = {}
 
 export const App: React.FC<AppProps> = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    data: []
+  })
+
+  useEffect(() => {
+    getingredients();
+  }, []);
+
+  const getingredients = () => {
+    setState({ ...state, hasError: false, isLoading: true });
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(data => setState({ ...state, data, isLoading: false }))
+      .catch(e => {
+        setState({ ...state, hasError: true, isLoading: false });
+      });
+  };
+
   return (
     <>
       <AppHeader/>
